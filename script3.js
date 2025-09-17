4eca028be7ab3aa5ecb12c111147c3917241ad26
sgellest <sarahgsteichmann06@gmail.com>
2025-09-17 19:55:21 -0300

    Modificação do código

--- script3.js
+++ script3.js
@@ -1,6 +1,9 @@
 //Operador Ternário – Notas da Escola
 //Um aluno precisa de média ≥ 6 para ser aprovado. Dada a nota, use o operador ternário para exibir:
 
-let nota = 6.0
+let nota1 = 6.0
+let nota2 = 5.0
 
-console.log(nota>= 6 ? "O aluno está aprovado!" : "O aluno está reprovado!")
+let media = (nota1+nota2)/2
+
+console.log(media>= 6 ? "O aluno está aprovado!" : "O aluno está reprovado!")
